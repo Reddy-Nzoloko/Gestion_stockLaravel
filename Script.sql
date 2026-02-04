8a688d4370aec93a42d355474608c4a406dbccd8
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-04 07:20:47 +0200

    Update Script.sql
    Rappel sur le trigger

--- Script.sql
+++ Script.sql
@@ -3,5 +3,6 @@ create DATABASE gestion_stockLaravel
 use gestion_stockLaravel;
 SELECT * FROM products;
 insert into pruducts where ....
--- le commentaire pour faire le repo de la journee 
+-- le commentaire pour faire le repo de la journée
+-- la creation des triggers ne doit pas manquer  
 
